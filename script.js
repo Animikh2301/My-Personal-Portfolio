9e6ff316cfd1dbe36deb66acda098ae236c4d5ec
Animikh2301 <animikhghoshsepco@gmail.com>
2024-03-27 21:30:29 +0530

    Services section modified

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 //==================== typed js ============================//
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer','Final Year Student','Python Developer'],
+    strings: ['Full Stack Developer','Final Year Student','Tech Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 2000,
